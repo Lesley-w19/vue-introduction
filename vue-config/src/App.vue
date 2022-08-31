602832dd10ce1b01a4c467bcd38159c33875ccdf
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-08-31 18:21:30 +0300

    Added the .gitignore file

---
+++ vue-config/src/App.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello, Button COUNTER!",
+      count: 0,
+
+      parentMessage: "Parent",
+      items: [
+        { id: 1, title: "Element 1", message: "Foo" },
+        { id: 2, title: "Element 2", message: "Bar" },
+      ],
+      myObject: {
+        title: "How to do lists in Vue",
+        author: "Jane Doe",
+        publishedAt: "2016-04-10",
+      },
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="message">{{ message }}</div>
+    <button @click="count++" class="btn">Count</button>
+    <p>The count is {{ count }}</p>
+
+    <!-- <button v-bind:disabled="isbuttonDisabled">Button</button> -->
+    <ul>
+      <!-- list rendering -->
+      <li v-for="(item, index) in items">
+        {{ item.id }}. {{ item.title }} is the {{ parentMessage }} -
+        {{ index }} - {{ item.message }}
+      </li>
+
+      <!-- list rendering using keys -->
+      <li v-for="item in myObject">
+        {{ item }}
+      </li>
+
+      <li v-for="(item, key) in myObject">{{ key }} : {{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<template v-for="item in items">
+  <li>{{ item.msg }}</li>
+</template>
+
+<style lang="scss">
+@import "./styles.scss";
+</style>
---
+++ vue-config/src/index.js
@@ -0,0 +1,15 @@
+import * as Vue from "vue";
+import * as VueRouter from "vue-router";
+import App from "./App.vue";
+
+const routes = [{ path: "/", component: App }];
+
+const router = VueRouter.createRouter({
+  routes,
+  history: VueRouter.createWebHistory(),
+});
+
+const app = Vue.createApp(App);
+
+app.use(router);
+app.mount("#root");
